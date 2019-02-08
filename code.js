7449bb2ae3e314f66e64bd9fdd353feb35b809df
nunna19 <nunnapat_ch@yahoo.com>
2019-02-08 09:48:42 -0500

    five

--- code.js
+++ code.js
@@ -1,16 +1,25 @@
-// window.onload = function() {
-// document.querySelector('.start-btn').onclick = function(){
-// startGame()
-// };
+let ANIME;
+let gameOver = false; 
+window.onload = function() {
+  document.querySelector('.start-btn').onclick = function(){
+    window.clearInterval(ANIME)
+    ANIME = window.setInterval(animate, 100)
+    songStartPlay.play()
+    setInterval(arrayboy,200)
+  }
+}
+
 var canvas = document.querySelector('.gameBackground');
 var ctx = canvas.getContext('2d');
       canvas.width = 1100;
       canvas.height = 550;
+     
+        
 
-    
 
 
 
+//////////////.............................................HERO........................................////////////
 
   var mouse ={};
   window.addEventListener('mousemove',function(event){
@@ -20,8 +29,10 @@ var ctx = canvas.getContext('2d');
   
          if (mouse.x - 150 > hero.x){
           img.src = 'img/rightmissite.png';
+          // songStartPlay.play()
            }else {
            img.src = 'img/leftmissite.png';
+          //  songStartPlay.play()
              }
   })
 
@@ -54,24 +65,23 @@ var ctx = canvas.getContext('2d');
 
 
 
+/////////.............................................METEOR.............................................//////////
 
 
-
-  var ballOriginal ={
-    radius : 20,
+  var ball ={
+    radius : 100,
     x : Math.random() * canvas.width,
     y : 0,
     speedDicrY : 0.01,
-    star :[],
-    ballBounce : 0.8,
-    gravity : 1,
-    floor: 500
+    gravity : 0.5,
+    floor: 510,
+    speed:20
    }
+
    var arrBall =[]
 
-
-  for(let i=0; i<10; i++){ //create all balls 
-    let eachBall = {...ballOriginal, y:i*-100, x:Math.random()*canvas.width}
+  for(let i=0; i<1000; i++){ //create all balls 
+    let eachBall = { ...ball , y:i*-100, x:Math.random()*canvas.width}
     arrBall.push(eachBall)
   }
 
@@ -79,58 +89,102 @@ var ctx = canvas.getContext('2d');
       for(let j=0; j<arrBall.length; j++){
         console.log()
         drawBall(arrBall[j],j)
+            
       }
    }
+   var xImgBall,yImgBall,widthImgBall,heightImgBall,scaleImgBall,speedImgBall,spriteWidthImgBall,spriteHeightImgBall,rowsImgBall,colsImgBall,curFrameImgBall,frameCountImgBall,srcXImgBall,srcYImgBall;
+
+   spriteWidthImgBall= 539;
+   spriteHeightImgBall =92;
+   
+   rowsImgBall= 1;
+   colsImgBall= 5;
+   
+   curFrameImgBall= 0
+   frameCountImgBall = 5
+   
+   srcYImgBall= 0;
+   srcXImgBall= 0;
+   
+   widthImgBall = spriteWidthImgBall/colsImgBall;
+   heightImgBall = spriteHeightImgBall/rowsImgBall;
+  
+   trackRightImgBall= 0;
 
 
    
+  var point = 20;
+  var score = 0;
+   var ImgBall= new Image(); 
+   ImgBall.src = "img/ball.gif";
+                     
   function drawBall(ball,index){ //draw each ball 
            
-            
-              ctx.beginPath();
-              ctx.arc(ball.x,ball.y,ball.radius,0, Math.PI*2,false ); 
-
-              ctx.fillStyle = 'blue'; 
-              ctx.stroke();
-          
-
+   
+              ctx.drawImage(ImgBall,srcXImgBall,srcYImgBall,widthImgBall,heightImgBall,ball.x,ball.y,ball.radius,heightImgBall);
+              curFrameImgBall = ++curFrameImgBall % frameCountImgBall; 
+              srcXImgBall = curFrameImgBall * widthImgBall; 
+              srcYImgBall = trackRightImgBall * heightImgBall; 
+              
+           
           
             if (ball.x < hero.x + hero.imgWidth &&
-                  ball.x + 2*ball.radius > hero.x &&
+                  ball.x + 1*ball.radius > hero.x &&
                   ball.y < hero.y + hero.imgHeight &&
-                  2*ball.radius + ball.y > hero.y)
-                  {drawEffect(ball)
-                   arrBall.splice(index,1)
-                   ctx.fillStyle(ball.x,ball.y,ball.width,ball.height);
+                  1*ball.radius + ball.y > hero.y)
+                  {
+                    drawEffect(ball) 
+                    bombSound.play()
+                    score += point;
+                    document.querySelector('.curScore').innerHTML= score
+                    arrBall.splice(index,1)
+                    
 
               }else if (ball.x < x2 + width2 &&
                         ball.x + 2*ball.radius > x2 &&
                         ball.y < y2 + height2 &&
                         2*ball.radius + ball.y > y2){
-                          console.log('boom')
-                         carDrawEffectF()           
-                         height2 = 20
-                         y2=490;
-                         x2 +=10                 
+                         carDrawEffectF() 
+                         crushFerrari()          
+                          
+
                 }else if (ball.x < x3 + width3 &&
                          ball.x + 2*ball.radius > x3 &&
                          ball.y < y3 + height3 &&
                          2*ball.radius + ball.y > y3){
-                        console.log('boom')
-                       carDrawEffectF()           
-                       height3 = 20;
-                       y3 =500;
-                       x3 -=10;       
+                        
+                        carDrawEffectPF()           
+                       crushPontiacFirebird()
+                      
                 }
 
-         if( ball.y + ball.radius +ball.speedDicrY > ball.floor){
-            console.log('game over')
+         if( ball.y + ball.radius  > ball.floor){
+            //console.log('game over')
             drawEffect(ball)
-           
-        
-          }else {
-              ball.speedDicrY += ball.gravity
-              ball.y += ball.speedDicrY
+              if (index < 1 ){
+                setTimeout(womenScream.play(),200)
+              }else{
+                womenScream.pause()              
+              }
+
+              if (index < 10 ){
+                  setTimeout(bombSound.play(),200)
+              }else{
+                bombSound.pause()
+              }setTimeout(drawGameOver, songStartPlay.pause(), 500)
+
+              if(!gameOver) {
+                gameOverSound.play()
+                //clearInterval(ANIME)
+                gameOver = true;
+                canvas.addEventListener('click', function() { 
+                  window.location.reload()
+                }, false);
+                document.body.append()
+              }
+                                   
+          }else {           
+              ball.y += ball.speed
               ball.x += 3;
             }
 
@@ -148,12 +202,70 @@ var ctx = canvas.getContext('2d');
 
 
 
+///////..................................................BOMB........................................................///////
+
+var x1,y1,spriteWidth1, spriteHeight1, rows1,cols1,width1,height1,curFrame1,frameCount1,srcX1,srcY1,scale1,
+spriteWidth1 = 489; 
+spriteHeight1 = 49;
+
+rows1 = 1; 
+cols1 = 10
+
+trackRight1 = 0; 
 
 
-///////////////////////////////////////////////Character Of Game//////////////////////////////////////////////
+width1 = spriteWidth1/cols1; 
+height1 = spriteHeight1/rows1
+
+curFrame1 = 0; 
+frameCount1 = 9; 
+
+x1=0;
+y1=405
+
+srcX1; 
+srcY1;
+
+scale1 = 3
 
 
-var x,y,spriteWidth, spriteHeight, rows,cols,trackRight,trackLeft,width,height,curFrame,frameCount,leftFrame,srcX,srcY,scale,speed;
+var imgEffect = new Image(); 
+imgEffect.src = "img/Effect2.png";
+
+               function updateEffect(){
+
+               curFrame1 = ++curFrame1 % frameCount1; 
+               srcX1 = curFrame1 * width1; 
+               srcY1 = trackRight1 * height1; 
+               ctx.clearRect(x1,y1,width1,height1); 
+               
+               }
+               function drawEffect(ball){
+                 updateEffect()
+                 
+               ctx.drawImage(imgEffect,srcX1,srcY1,width1,height1,ball.x-20,ball.y-30,width1*scale1 ,height1*scale1 );
+               }
+               function carDrawEffectF(){
+                 updateEffect()
+               ctx.drawImage(imgEffect,srcX1,srcY1,width1,height1,x2,y2-90,width1*scale1 ,height1*scale1 );
+               }
+               function carDrawEffectPF(){
+                 updateEffect()
+               ctx.drawImage(imgEffect,srcX1,srcY1,width1,height1,x3,y3-90,width1*scale1 ,height1*scale1 );
+               }
+       
+
+
+
+
+
+
+
+
+
+//////////////.............................................BOY RUN.......................................................///////////
+
+var x,y,spriteWidth, spriteHeight, rows,cols,trackRight,width,height,curFrame,frameCount,srcX,srcY,scale,speed;
  
  spriteWidth = 864; 
  spriteHeight = 280
@@ -162,14 +274,13 @@ var x,y,spriteWidth, spriteHeight, rows,cols,trackRight,trackLeft,width,height,c
  cols = 8
  
  trackRight = 0; 
- trackLeft = 1
 
  width = spriteWidth/cols; 
  height = spriteHeight/rows
 
  curFrame = 0; 
  frameCount = 8; 
-leftFrame  = 6;
+
 
  x=0;
  y=405
@@ -178,7 +289,12 @@ leftFrame  = 6;
  srcY=0
 
  scale = 0.5
- speed = 12; 
+ speed = 10; 
+ 
+ 
+
+
+
  
  var character = new Image(); 
  character.src = "img/character.png";
@@ -192,99 +308,77 @@ leftFrame  = 6;
                     x+=speed;
                 }	
 
-                function updateFrameLeft(){
-                  leftFrame = ++leftFrame % frameCount; 
-                  srcX = leftFrame * width; 	
-                  srcY = trackLeft * height; 
-                  ctx.clearRect(x,y,width,height);
-                    x-=speed;
-                }
-                
-
-
-                function walkBack(){
-                  if(x >= 0){
-                    updateFrameRight()
-                    //console.log('right work')
-                    }else if (x <= 600) {
-                      updateFrameLeft()
-                    //console.log('left workkkkk')
-                    }
-                    
-                }
                 function drawBoy(){
-                walkBack()
+                  updateFrameRight()
                 ctx.drawImage(character,srcX,srcY,width,height,x,y,width*scale ,height*scale );
                 }
                 
+
+
+
+
+
+
              
+/////....................................................Boy2....................................................////////
+var x4,y4,width4,height4,scale4,speed4;
+width4 = 40;
+height4 = 70;
 
+x4=550;
+y4=400;
 
+scale4 = 3;
+speed4 = 12; 
 
+var imgBoy2 = new Image(); 
+imgBoy2.src = "img/Boy-Game-Sprites.png";
 
-
- var x1,y1,spriteWidth1, spriteHeight1, rows1,cols1,width1,height1,curFrame1,frameCount1,srcX1,srcY1,scale1,
- spriteWidth1 = 489; 
- spriteHeight1 = 49;
-
- rows1 = 1; 
- cols1 = 10
- 
- trackRight1 = 0; 
- trackLeft1 = 1
-
- width1 = spriteWidth1/cols1; 
- height1 = spriteHeight1/rows1
-
- curFrame1 = 0; 
- frameCount1 = 9; 
-
- x1=0;
- y1=405
-
- srcX1; 
- srcY1;
-
- scale1 = 3
- speed1 = 12; 
- 
- var imgEffect = new Image(); 
- imgEffect.src = "img/Effect2.png";
- 
-                function updateEffect(){
-
-                curFrame1 = ++curFrame1 % frameCount1; 
-                srcX1 = curFrame1 * width1; 
-                srcY1 = trackRight1 * height1; 
-                ctx.clearRect(x1,y1,width1,height1); 
+                function drawBoy2(){
+                ctx.drawImage(imgBoy2,x4,y4,width4,height4);
                 
-                }
-                function drawEffect(ball){
-                  updateEffect()
-                ctx.drawImage(imgEffect,srcX1,srcY1,width1,height1,ball.x-50,ball.y-85,width1*scale1 ,height1*scale1 );
-                }
-                function carDrawEffectF(){
-                  updateEffect()
-                ctx.drawImage(imgEffect,srcX1,srcY1,width1,height1,x2,y2-100,width1*scale1 ,height1*scale1 );
-                }
-                function carDrawEffectPF(){
-                  updateEffect()
-                ctx.drawImage(imgEffect,srcX1,srcY1,width1,height1,x3,y3-100,width1*scale1 ,height1*scale1 );
-                }
+                  }
 
 
 
 
-var x2,y2,width2,height2,scale2,speed2,newX2;
-width2 = 150;
-height2 = 80;
+
+
+/////....................................................Girl....................................................////////
+
+var imgGirl1 = new Image(); 
+imgGirl1.src = "img/girl-left.png";
+
+                function drawGirl1(){
+                ctx.drawImage(imgGirl1,x4+70,y4-5,width4+20,height4+10);
+                
+                  }
+
+
+
+/////....................................................Police....................................................////////
+
+var imgBoy3 = new Image(); 
+imgBoy3.src = "img/Police-Character-.png";
+
+                function drawBoy3(){
+                ctx.drawImage(imgBoy3,x4+300,y4,width4,height4);
+                
+                  }
+
+/////////.................................................Ferrari...................................................////////
+
+
+var x2,y2,width2,height2,scale2,speed2;
+width2 = 140;
+height2 = 70;
 
 x2=canvas.width;
-newX2 = x2 - 2
+
 y2=445;
 
 scale2 = 3;
-speed2 = 10; 
+speed2 = 12; 
 
 var imgFerrari = new Image(); 
 imgFerrari.src = "img/ferrari.png";
@@ -292,10 +386,50 @@ imgFerrari.src = "img/ferrari.png";
                 function drawFerrari(){
                 ctx.drawImage(imgFerrari,x2,y2,width2,height2);
                 x2 -=speed2
-
                   }
+                function crushFerrari(){
+                  height2 = 20   // height of Ferrari
+                  y2=490;        // y of Ferrari
+                  x2 +=10        // x of Ferrarij
+                }
 
+
+
+
+
+
+
+/////////.............................................PontiacFirebird...................................................////////
  
+var x3,y3,width3,height3,scale3,speed3;
+width3 = 190;
+height3 = 80;
+
+x3=0;
+y3=470;
+
+scale3 = 3;
+speed3 = 9; 
+
+
+
+var imgPontiacFirebird= new Image(); 
+imgPontiacFirebird.src = "img/PontiacFirebird.png";
+
+                  
+                function drawPontiacFirebird(){
+                ctx.drawImage(imgPontiacFirebird,x3,y3,width3,height3);
+                x3 +=speed3
+               
+                }
+                function crushPontiacFirebird(){
+                  height3 = 20; // height of PontiacFirebird
+                  y3 =500;       // y  of PontiacFirebird
+                  x3 -= 10.5;      // x of PontiacFirebird
+                }
+               
+/////////.............................................BugattiVeyron...................................................////////
+
 
 var x3,y3,width3,height3,scale3,speed3;
 width3 = 190;
@@ -305,15 +439,39 @@ x3=0;
 y3=470;
 
 scale3 = 3;
-speed3 = 10; 
+speed3 = 9; 
+
+
 
 var imgPontiacFirebird= new Image(); 
-imgPontiacFirebird.src = "img/PontiacFirebird.png";
+imgPontiacFirebird.src = "img/BugattiVeyron.png";
+
                   
                 function drawPontiacFirebird(){
                 ctx.drawImage(imgPontiacFirebird,x3,y3,width3,height3);
                 x3 +=speed3
+               
+                }
 
+
+
+
+
+
+/////////////.............................................GAME OVER.................................................///////
+
+var xGameOver,yGameOver,widthGameOver,heightGameOver;
+widthGameOver = 500;
+heightGameOver = 400;
+
+xGameOver =270;
+yGameOver =10;
+var imgGameOver = new Image(); 
+imgGameOver.src = "img/GameOver.gif";
+
+                function drawGameOver(){
+                ctx.drawImage(imgGameOver,xGameOver,yGameOver,widthGameOver,heightGameOver);
+               
                   }
 
 
@@ -328,18 +486,63 @@ imgPontiacFirebird.src = "img/PontiacFirebird.png";
 
 
 
+///////........................................................SOUND.......................................................////////
+
+function sound(src) { 
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+}
+
+
+let bombSound = new Audio()
+let gameOverSound = new Audio()
+let womenScream = new Audio()
+let songStartPlay= new Audio()
+bombSound.src = "./img/8bit_bomb_explosion (online-audio-converter.com).mp3"
+gameOverSound.src = "./img/15879275_retro-game-synth-organ-game-over_by_ghetty_preview.mp3"
+womenScream.src ="./img/NFF-girl-scream (1) (online-audio-converter.com).mp3"
+songStartPlay.src ="./img/1-04 Desire.mp3"
+
+
+
+
+
+
+
 
 function animate(){ //draw everything in
+ 
   ctx.clearRect(0,0,canvas.width,canvas.height); //clear WHOLE canvas 
+  // setInterval(drawBoy,2000)
+  drawBoy()
+  drawGirl1()
+  drawBoy2()
+  drawBoy3()  
   
-              drawBoy()
-              drawFerrari()
+              setInterval(drawFerrari,10000)
               drawPontiacFirebird()
               drawHero()
               drawBalls()
-           
+              
+              
           }
-setInterval(animate, 100)
+
+
+
+       
+   
+
+        
 
 
 
